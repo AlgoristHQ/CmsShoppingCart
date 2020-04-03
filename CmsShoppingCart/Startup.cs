6f585c2e92645b558c244e897dc85eac69432f19
TheEngineeringAlgorist <55904047+TheEngineeringAlgorist@users.noreply.github.com>
2020-04-02 20:35:24 -0500

    Added EF

--- CmsShoppingCart/Startup.cs
+++ CmsShoppingCart/Startup.cs
@@ -50,7 +50,7 @@ namespace CmsShoppingCart
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=index}/{id?}");
             });
         }
     }
