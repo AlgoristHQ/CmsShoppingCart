888b7bae0f9296967bf7b9c177e3b819594c74c1
TheEngineeringAlgorist <55904047+TheEngineeringAlgorist@users.noreply.github.com>
2020-04-03 00:27:58 -0500

    Setup the admin pages controller to return all of the pages available to the admin

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -2,16 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CmsShoppingCart.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PagesController : Controller
     {
-        public string Index()
+        private CmsShoppingCartContext _context;
+        public PagesController(CmsShoppingCartContext context)
         {
-            return "test";
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in _context.Pages orderby p.Sorting select p;
+            List<Page> pagesList = await pages.ToListAsync();
+            return View(pagesList);
         }
     }
 }
