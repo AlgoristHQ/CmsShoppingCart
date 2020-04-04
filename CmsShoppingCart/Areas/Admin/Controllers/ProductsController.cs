a0027fb3aed1b4b536c9d38f9e89019a1d8b0865
TheEngineeringAlgorist <55904047+TheEngineeringAlgorist@users.noreply.github.com>
2020-04-04 10:41:29 -0500

    Created products table/model

---
+++ CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private CmsShoppingCartContext _context;
+        public ProductsController(CmsShoppingCartContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
